161f835227c22b1199e7369f3ec87c320e72bf5a
hritik <hchokker7@gmail.com>
2019-06-10 14:08:36 +0530

    done

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,4 @@
 const mongodb = require('../utils/env');
 const mongoose = require('mongoose');
-mongoose.connect(mongodb.db);
+mongoose.connect(mongodb.db,{useNewUrlParser: true});
 module.exports = mongoose;
